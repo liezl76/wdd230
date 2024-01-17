6e0ba3987b690ad632ee68845ad2fc9322f27786
liezl76 <131637313+liezl76@users.noreply.github.com>
2024-01-17 11:32:35 +0800

    add javascript in bom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,8 +1,25 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.querySelector("#favchap");
+    const button = document.querySelector("button");
+    const list = document.querySelector("#list");
 
-button.addEventListener('click', function() {});
-button.addEventListener('click', () => {});
+    button.addEventListener("click", function () {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
 
+        if (input.value.trim() !== "") {
+            li.textContent = input.value;
+            deleteButton.textContent = "X";
+            li.append(deleteButton);
+            list.append(li);
 
+            deleteButton.addEventListener("click", function () {
+                list.removeChild(li);
+                input.focus();
+            });
+
+            input.focus();
+            input.value = "";
+        }
+    });
+});
