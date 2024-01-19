5aeae9349567d5907e86cc56c6e946780e1bf114
liezl76 <liezl1106@gmail.com>
2024-01-19 10:14:10 +0800

    edit css and darkMode.js

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,14 +1,18 @@
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+document.addEventListener('DOMContentLoaded', function () {
+    const modeButton = document.getElementById('mode');
+    const main = document.querySelector('main');
+    const body = document.body;
 
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "🔆";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "🕶️";
-	}
+    modeButton.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            main.style.background = '#000';
+            main.style.color = '#fff';
+            modeButton.textContent = '🔆';
+        } else {
+            main.style.background = '#eee';
+            main.style.color = '#000';
+            modeButton.textContent = '🕶️';
+        }
+    });
 });
