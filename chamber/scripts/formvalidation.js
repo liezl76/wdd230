ae178617e48e4e0324369da169098c1d78d5a256
liezl76 <liezl1106@gmail.com>
2024-02-03 23:55:42 +0800

    edit join html

--- chamber/scripts/formvalidation.js
+++ chamber/scripts/formvalidation.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const timestampField = document.getElementById('timestamp');
+    const currentTimestamp = new Date().toISOString();
+    timestampField.value = currentTimestamp;
+});
+
 function validateForm() {
     // Existing form validation logic
 
