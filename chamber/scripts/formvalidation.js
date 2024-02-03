f8c7a19753633cb85eb4f2e8cce5c96ea5b8b06d
liezl76 <liezl1106@gmail.com>
2024-02-03 22:29:45 +0800

    add join html

---
+++ chamber/scripts/formvalidation.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    // Existing form validation logic
+
+    // If validation passes, redirect to record.html
+    window.location.href = 'record.html';
+
+    return false; // To prevent the default form submission
+}
