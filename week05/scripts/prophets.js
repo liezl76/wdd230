892679de098335af9b8b02e7adccf191add4dbea
liezl76 <131637313+liezl76@users.noreply.github.com>
2024-02-05 15:14:34 +0800

    edit prophet html

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -2,34 +2,40 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 const cards = document.querySelector('#cards');
 
 async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.prophets); 
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data.prophets); // temporary testing of data retrieval
+  displayProphets(data.prophets);
 }
 
 getProphetData();
 
 const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-    // Create element to add to the div.cards element
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
     let card = document.createElement('section');
     let fullName = document.createElement('h2');
+    let birthInfo = document.createElement('p'); //New paragraph for birthdate and birthplace
     let portrait = document.createElement('img');
 
-    //// Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${prophet.firstName} ${prophet.lastName}`; // fill in the blank
+    // Build the h2 content out to show the prophet's full name
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    //Build the paragraph for birth information
+    birthInfo.textContent = `Born: ${prophet.birthdate} in ${prophet.birthplace}`;
+
     // Build the image portrait by setting all the relevant attributes
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.firstName} ${prophet.lastName}`); // fill in the blank
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
 
     // Append the section(card) with the created elements
-    card.appendChild(fullName); //fill in the blank
+    card.appendChild(fullName);
+    card.appendChild(birthInfo); //Append the birth information
     card.appendChild(portrait);
 
     cards.appendChild(card);
-  }); // end of arrow function and forEach loop   
+  }); // end of arrow function and forEach loop
 }
