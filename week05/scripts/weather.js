dbd0231a5865d68e047182404bdc25883a6a0542
liezl76 <131637313+liezl76@users.noreply.github.com>
2024-02-07 15:43:22 +0800

    edit weather html

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,8 +1,6 @@
 // select HTML elements in the document
-const myTown = document.querySelector('#town');
-const myDescription = document.querySelector('#description')
-const myTemperature = document.querySelector('#temperature');
-const myGraphic = document.querySelector('#graphic')
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 // Define the URL for the OpenWeatherMap API
@@ -37,13 +35,14 @@ async function apiFetch() {
   }
 }
 
-// // Function to display results in the HTML document
+// Function to display results in the HTML document
 function displayResults(data) {
-  myDescription.innerHTML = data.weather[0].myDescription
-  myTemperature.innerHTML = `${data.main.temp}&deg;F`
-  const iconsrc = 'https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'
-  myGraphic.setAttribute('src', iconsrc)
-  myGraphic.setAttribute('alt', data.weather[0].description)
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = 'https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png';
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', data.weather[0].description);
+  captionDesc.textContent = `${desc}`;
 }
 
 // Call the apiFetch function to fetch data from the API
