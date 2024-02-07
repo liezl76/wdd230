1e473f3d8ff44d8f81e224a8590914d2f252fa2a
liezl76 <liezl1106@gmail.com>
2024-02-07 19:33:43 +0800

    edit index html

--- scripts/pageCounter.js
+++ scripts/pageCounter.js
@@ -1,3 +1,56 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// Define the URL for the OpenWeatherMap API
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Define the API key
+const apiKey = '998fbd78ab14ba0ce1f98c993ab57a6f';
+
+// Define the latitude and longitude of Trier, Germany
+const latitude = '10.59637';
+const longitude = '122.58735';
+
+// Define the units
+const units = 'imperial';
+
+// Construct the URL with query parameters
+const apiUrl = `//api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+
+// Define an asynchronous function to fetch data from the API
+async function apiFetch() {
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // display results in HTML
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Function to display results in the HTML document
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = 'https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png';
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', data.weather[0].description);
+  captionDesc.textContent = `${desc}`;
+}
+
+// Call the apiFetch function to fetch data from the API
+apiFetch();
+
+
+
+
 // 1️⃣ Initialize display element variable
 const visitsDisplay = document.querySelector(".visits");
 
