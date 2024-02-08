defdb97c9dd09bfc205d8eb2c617b2ba9cc3fcec
liezl76 <liezl1106@gmail.com>
2024-02-08 10:29:19 +0800

    add links.js

--- week05/scripts/links.js
+++ week05/scripts/links.js
@@ -1,2 +1,16 @@
-const baseURL = "";
-const linksURL = "";
+const baseURL = "https://github.com/liezl76/wdd230/";
+const linksURL = "https://github.com/liezl76/wdd230/blob/main/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
